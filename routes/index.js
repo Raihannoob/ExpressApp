b883821d228867bc0864dda70a821286ba7ec083
Raihannoob <raihan.tanvir961@gmail.com>
2021-12-30 15:32:13 +0600

    Fix integer issue

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ router.get("/all/:country", function (req, res, next) {
       const far2 = [];
       const far3 = [];
       const far4 = [];
-     
+    //  time 1
       for(var i =0; i<6; i++) {
           console.log(forecast[0].hour[i].temp_c);
           val1.push(forecast[0].hour[i].temp_c)
@@ -41,8 +41,8 @@ router.get("/all/:country", function (req, res, next) {
         const sort1 = val1.sort();
         console.log(sort1[0]);
         console.log(sort1[sort1.length-1]); 
-        const low1 = parseInt(sort1[0]);
-        const high1 = parseInt(sort1[sort1.length-1]);
+        const low1 = Math.round(sort1[0]);
+        const high1 = Math.round(sort1[sort1.length-1]);
       //  for farhinite
       for(var i =0; i<6; i++) {
         console.log(forecast[0].hour[i].temp_f);
@@ -51,8 +51,8 @@ router.get("/all/:country", function (req, res, next) {
       const sortf1 = far1.sort();
       console.log(sortf1[0]);
       console.log(sortf1[sortf1.length-1]); 
-      const lowf1 = parseInt(sortf1[0]);
-      const highf1 = parseInt(sortf1[sortf1.length-1]);
+      const lowf1 = Math.round(sortf1[0]);
+      const highf1 = Math.round(sortf1[sortf1.length-1]);
         // time 2
       for(var i =6; i<12; i++) {
           console.log(forecast[0].hour[i].temp_c);
@@ -61,8 +61,8 @@ router.get("/all/:country", function (req, res, next) {
         const sort2 = val2.sort();
         console.log(sort2[0]);
         console.log(sort2[sort2.length-1]); 
-        const low2 =parseInt( sort2[0]);
-        const high2 = parseInt(sort2[sort2.length-1]);
+        const low2 =Math.round( sort2[0]);
+        const high2 = Math.round(sort2[sort2.length-1]);
       //for ferhinite
       for(var i =6; i<12; i++) {
         console.log(forecast[0].hour[i].temp_f);
@@ -71,8 +71,8 @@ router.get("/all/:country", function (req, res, next) {
       const sortf2 = far2.sort();
       console.log(sortf2[0]);
       console.log(sortf2[sortf2.length-1]); 
-      const lowf2 =parseInt( sortf2[0]);
-      const highf2 = parseInt(sortf2[sortf2.length-1]);
+      const lowf2 = Math.round( sortf2[0]);
+      const highf2 = Math.round(sortf2[sortf2.length-1]);
 
         // time 3 
       for(var i =12; i<18; i++) {
@@ -82,8 +82,8 @@ router.get("/all/:country", function (req, res, next) {
         const sort3 = val3.sort();
         console.log(sort3[0]);
         console.log(sort3[sort3.length-1]); 
-        const low3 = parseInt(sort3[0]);
-        const high3 =parseInt(sort3[sort3.length-1]);
+        const low3 = Math.round(sort3[0]);
+        const high3 = Math.round(sort3[sort3.length-1]);
         // for ferhinite
       for(var i =12; i<18; i++) {
           console.log(forecast[0].hour[i].temp_f);
@@ -92,8 +92,8 @@ router.get("/all/:country", function (req, res, next) {
         const sortf3 = far3.sort();
         console.log(sortf3[0]);
         console.log(sortf3[sortf3.length-1]); 
-        const lowf3 = parseInt(sortf3[0]);
-        const highf3 =parseInt(sortf3[sortf3.length-1]);  
+        const lowf3 = Math.round(sortf3[0]);
+        const highf3 = Math.round(sortf3[sortf3.length-1]);  
         // time4
       for(var i =18; i<24; i++) {
         console.log(forecast[0].hour[i].temp_c);
@@ -102,8 +102,8 @@ router.get("/all/:country", function (req, res, next) {
         const sort4 = val4.sort();
         console.log(sort4[0]);
         console.log(sort4[sort4.length-1]); 
-        const low4 = parseInt(sort4[0]);
-        const high4 = parseInt(sort4[sort4.length-1]);
+        const low4 = Math.round(sort4[0]);
+        const high4 = Math.round(sort4[sort4.length-1]);
         // for farhinite
         for(var i =18; i<24; i++) {
           console.log(forecast[0].hour[i].temp_f);
@@ -112,8 +112,8 @@ router.get("/all/:country", function (req, res, next) {
           const sortf4 = far4.sort();
           console.log(sortf4[0]);
           console.log(sortf4[sortf4.length-1]); 
-          const lowf4 = parseInt(sortf4[0]);
-          const highf4 = parseInt(sortf4[sortf4.length-1]);
+          const lowf4 = Math.round(sortf4[0]);
+          const highf4 = Math.round(sortf4[sortf4.length-1]);
         res.render("index", { High1: high1, low1 : low1,High2:high2,low2:low2, High3:high3,low3:low3,High4:high4,low4:low4,Highf1:highf1,lowf1:lowf1,Highf2:highf2,lowf2:lowf2,Highf3:highf3,lowf3:lowf3,Highf4:highf4,lowf4:lowf4 });
     
     })
@@ -168,8 +168,8 @@ router.get("/all/:country/:city", function (req, res, next) {
         const sort1 = val1.sort();
         console.log(sort1[0]);
         console.log(sort1[sort1.length-1]); 
-        const low1 = parseInt(sort1[0]);
-        const high1 = parseInt(sort1[sort1.length-1]);
+        const low1 = Math.round(sort1[0]);
+        const high1 = Math.round(sort1[sort1.length-1]);
       //  for farhinite
       for(var i =0; i<6; i++) {
         console.log(forecast[0].hour[i].temp_f);
@@ -178,8 +178,8 @@ router.get("/all/:country/:city", function (req, res, next) {
       const sortf1 = far1.sort();
       console.log(sortf1[0]);
       console.log(sortf1[sortf1.length-1]); 
-      const lowf1 = parseInt(sortf1[0]);
-      const highf1 = parseInt(sortf1[sortf1.length-1]);
+      const lowf1 = Math.round(sortf1[0]);
+      const highf1 = Math.round(sortf1[sortf1.length-1]);
         // time 2
       for(var i =6; i<12; i++) {
           console.log(forecast[0].hour[i].temp_c);
@@ -188,8 +188,8 @@ router.get("/all/:country/:city", function (req, res, next) {
         const sort2 = val2.sort();
         console.log(sort2[0]);
         console.log(sort2[sort2.length-1]); 
-        const low2 =parseInt( sort2[0]);
-        const high2 = parseInt(sort2[sort2.length-1]);
+        const low2 = Math.round( sort2[0]);
+        const high2 = Math.round(sort2[sort2.length-1]);
       //for ferhinite
       for(var i =6; i<12; i++) {
         console.log(forecast[0].hour[i].temp_f);
@@ -198,8 +198,8 @@ router.get("/all/:country/:city", function (req, res, next) {
       const sortf2 = far2.sort();
       console.log(sortf2[0]);
       console.log(sortf2[sortf2.length-1]); 
-      const lowf2 =parseInt( sortf2[0]);
-      const highf2 = parseInt(sortf2[sortf2.length-1]);
+      const lowf2 =Math.round( sortf2[0]);
+      const highf2 = Math.round(sortf2[sortf2.length-1]);
 
         // time 3 
       for(var i =12; i<18; i++) {
@@ -209,8 +209,8 @@ router.get("/all/:country/:city", function (req, res, next) {
         const sort3 = val3.sort();
         console.log(sort3[0]);
         console.log(sort3[sort3.length-1]); 
-        const low3 = parseInt(sort3[0]);
-        const high3 =parseInt(sort3[sort3.length-1]);
+        const low3 = Math.round(sort3[0]);
+        const high3 = Math.round(sort3[sort3.length-1]);
         // for ferhinite
       for(var i =12; i<18; i++) {
           console.log(forecast[0].hour[i].temp_f);
@@ -219,8 +219,8 @@ router.get("/all/:country/:city", function (req, res, next) {
         const sortf3 = far3.sort();
         console.log(sortf3[0]);
         console.log(sortf3[sortf3.length-1]); 
-        const lowf3 = parseInt(sortf3[0]);
-        const highf3 =parseInt(sortf3[sortf3.length-1]);  
+        const lowf3 = Math.round(sortf3[0]);
+        const highf3 = Math.round(sortf3[sortf3.length-1]);  
         // time4
       for(var i =18; i<24; i++) {
         console.log(forecast[0].hour[i].temp_c);
@@ -229,8 +229,8 @@ router.get("/all/:country/:city", function (req, res, next) {
         const sort4 = val4.sort();
         console.log(sort4[0]);
         console.log(sort4[sort4.length-1]); 
-        const low4 = parseInt(sort4[0]);
-        const high4 = parseInt(sort4[sort4.length-1]);
+        const low4 = Math.round(sort4[0]);
+        const high4 = Math.round(sort4[sort4.length-1]);
         // for farhinite
         for(var i =18; i<24; i++) {
           console.log(forecast[0].hour[i].temp_f);
@@ -239,8 +239,8 @@ router.get("/all/:country/:city", function (req, res, next) {
           const sortf4 = far4.sort();
           console.log(sortf4[0]);
           console.log(sortf4[sortf4.length-1]); 
-          const lowf4 = parseInt(sortf4[0]);
-          const highf4 = parseInt(sortf4[sortf4.length-1]);
+          const lowf4 = Math.round(sortf4[0]);
+          const highf4 = Math.round(sortf4[sortf4.length-1]);
         res.render("index", { High1: high1, low1 : low1,High2:high2,low2:low2, High3:high3,low3:low3,High4:high4,low4:low4,Highf1:highf1,lowf1:lowf1,Highf2:highf2,lowf2:lowf2,Highf3:highf3,lowf3:lowf3,Highf4:highf4,lowf4:lowf4 });
     }
     else{
